d9bf189747e8cf083317084d299e032a7c8e00b6
ArcoKim <arcomade12@gmail.com>
2024-02-08 00:20:15 +0900

    24.02.08 : flask-dynamodb-api update

--- flask-dynamodb-api/app.py
+++ flask-dynamodb-api/app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify, make_response, request
 
 app = Flask(__name__)
 
-dynamodb_client = boto3.client('dynamodb')
+dynamodb_client = boto3.resource('dynamodb')
 
 if os.environ.get('IS_OFFLINE'):
     dynamodb_client = boto3.client(
@@ -23,7 +23,7 @@ def create_user():
         return jsonify({'error': 'Please provide both "userId" and "name"'}), 400
 
     table.put_item(
-        Item={'userId': {'S': user_id}, 'name': {'S': name}}
+        Item={'userId': user_id, 'name':  name}
     )
 
     return jsonify({'userId': user_id, 'name': name})
@@ -31,14 +31,14 @@ def create_user():
 @app.route('/users/<string:user_id>', methods=['GET'])
 def get_user(user_id):
     result = table.get_item(
-        Key={'userId': {'S': user_id}}
+        Key={'userId': user_id}
     )
     item = result.get('Item')
     if not item:
         return jsonify({'error': 'Could not find user with provided "userId"'}), 404
 
     return jsonify(
-        {'userId': item.get('userId').get('S'), 'name': item.get('name').get('S')}
+        {'userId': item.get('userId'), 'name': item.get('name')}
     )
 
 @app.route('/users/<string:user_id>', methods=['PATCH'])
@@ -48,8 +48,11 @@ def update_user(user_id):
         return jsonify({'error': 'Please provide "name"'}), 400
     
     result = table.update_item(
-        Key={'userId': {'S': user_id}},
-        UpdateExpression='SET name = :val1',
+        Key={'userId':  user_id},
+        UpdateExpression='SET #attr1 = :val1',
+        ExpressionAttributeNames={
+            "#attr1": "name"
+        },
         ExpressionAttributeValues={
             ':val1': name
         }
@@ -64,7 +67,7 @@ def update_user(user_id):
 @app.route('/users/<string:user_id>', methods=['DELETE'])
 def delete_user(user_id):
     result = table.delete_item(
-        Key={'userId': {'S': user_id}}
+        Key={'userId': user_id}
     )
     if not result:
         return jsonify({'error': 'Failed to delete user'}), 500
